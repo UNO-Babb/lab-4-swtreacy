0f39ca091893b4ab5fb8159ceb82bbed6c537892
swtreacy <swtreacy@gmail.com>
2024-09-22 10:47:46 -0500

    Finished Lab 4

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,7 +1,7 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name: Sean Treacy
+#Date: 9/22/24
+#Assignment: Turtle Graphics.py
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
@@ -10,6 +10,62 @@ def drawSquare(myTurtle, size):
     for i in range(4):
         myTurtle.forward(size)
         myTurtle.right(90)
+
+def drawPolygon(bob, sides):
+    for s in range(sides):
+        bob.forward(50)
+        bob.right(360/sides)
+
+def fillCorner(dave, corner):
+    #draw big square
+    drawSquare(dave, 100)
+    
+    if corner == 1:
+        dave.begin_fill()
+        drawSquare(dave, 50)
+        dave.end_fill()
+    elif corner == 2:
+        dave.penup()
+        dave.forward(50)
+        dave.pendown()
+        dave.begin_fill()
+        drawSquare(dave, 50)
+        dave.end_fill()
+    elif corner == 3:
+        dave.penup()
+        dave.right(90)
+        dave.forward(50)
+        dave.left(90)
+        dave.pendown()
+        dave.begin_fill()
+        drawSquare(dave, 50)
+        dave.end_fill()
+    elif corner == 4:
+        dave.penup()
+        dave.forward(50)
+        dave.right(90)
+        dave.forward(50)
+        dave.left(90)
+        dave.pendown()
+        dave.begin_fill()
+        drawSquare(dave, 50)
+        dave.end_fill()
+
+def squaresInSquares(chet, squares):
+    chet.up()
+    chet.goto(-200,200)
+    chet.down()
+    for s in range(squares):
+        drawSquare(chet, 40 * squares)
+        chet.up()
+        chet.forward(20)
+        chet.right(90)
+        chet.forward(20)
+        chet.left(90)
+        chet.down()
+        squares = squares - 1
+       
+        
 
 
 def main():
@@ -21,7 +77,7 @@ def main():
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+    #squaresInSquares(myTurtle, 3) #draws 3 concentric squares
 
 
 main()
